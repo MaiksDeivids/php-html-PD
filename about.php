5560ee9d2d598632ba41619f54dfad9568692068
You <you@example.com>
2025-12-05 15:33:54 +0200

    added tag p

--- about.php
+++ about.php
@@ -1,1 +1,27 @@
 <!-- your code goes here -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+    <li><a href="index.html">Home</a></li>
+    </ul>
+
+
+    <?php
+    $texts = [
+        ["Lorem ipsum dolor sit amet."],
+        ["Lorem ipsum dolor sit amet, consectetur adipisicing."],
+        ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iusto."],
+    ];
+
+    foreach($texts as $text);
+        echo "<p> $text </p>";
+    ?>
+</body>
+</html>
